edfb4336ddad4206d3a4af757eaf8703780f081c
KenSeong <kheys60@gmail.com>
2022-02-02 09:18:40 +0100

    Revert "commit mistake"

--- src/app/compte/compte.component.ts
+++ src/app/compte/compte.component.ts
@@ -1,28 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { users } from '../models/users.model';
-
 
 @Component({
   selector: 'app-compte',
   templateUrl: './compte.component.html',
   styleUrls: ['./compte.component.css']
 })
-
 export class CompteComponent implements OnInit {
-  name='John Doe';
-  localisation='Creil';
-  imgprofil='/assets/img/PhotoProfil.jpg';
 
-  
-  user:users|null=null;
-  
-  constructor(private router:Router, private route:ActivatedRoute) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(param=>{
-    this.user = 
-    })
   }
 
 }
