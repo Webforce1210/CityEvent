d6e229faf7fbbbc7f2ccb2d22d03273bfcf6ce60
Céline Foucart <celinefoucart@yahoo.fr>
2022-04-06 17:01:39 +0200

    Mes activités template html --start

--- src/app/mes-activites/mes-activites.component.ts
+++ src/app/mes-activites/mes-activites.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../models/User.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-mes-activites',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mes-activites.component.css']
 })
 export class MesActivitesComponent implements OnInit {
+  user!: User;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.checkSession();
   }
 
+  private checkSession(): void {
+    try {
+      const id: string = this.route.snapshot.params['userid'];
+      this.user = this.userService.findUserById(id);
+    } catch (error) {
+      this.router.navigateByUrl('/login');
+    }
+  }
 }
