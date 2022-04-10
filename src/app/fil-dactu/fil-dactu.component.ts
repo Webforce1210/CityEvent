7d3dbd0b3e7c0874cd27777805ecdab016b09eb6
Florent <miquel.florent@gmail.com>
2022-04-11 00:20:54 +0200

    flo1104

--- src/app/fil-dactu/fil-dactu.component.ts
+++ src/app/fil-dactu/fil-dactu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fil-dactu',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fil-dactu.component.css']
 })
 export class FilDactuComponent implements OnInit {
+
+  @Input() userId!: string;
 
   constructor() { }
 
