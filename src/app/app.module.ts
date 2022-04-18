91516e04864c6b7879f0223091d379c0b6354509
Florent <miquel.florent@gmail.com>
2022-04-18 23:33:35 +0200

    httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import httpClientModule, { HttpClientModule } from '@angular/common/http';
 
 import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
@@ -77,7 +78,8 @@ registerLocaleData(localeFr, 'fr');
     ReactiveFormsModule,
     FormsModule, 
     CreditCardDirectivesModule,
-    PickerModule
+    PickerModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
